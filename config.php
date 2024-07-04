d8a123b2bed5c85df5dc4064b744fe2cb62643af
SaadiDK-003 <pcremote059@gmail.com>
2024-07-05 03:01:45 +0500

    update

--- config.php
+++ config.php
@@ -2,7 +2,6 @@
 session_start();
 $percentage = 0.1;
 $fixed = 3.99;
-$url_prefix = $_SERVER['HTTP_HOST'] == 'localhost' ? 'http://':'https://';
-define('SITE_URL', ($url_prefix).$_SERVER['HTTP_HOST'].'/Payyourboostbill');
-echo SITE_URL;
+$url_prefix = $_SERVER['HTTP_HOST'] == 'localhost' ? 'http://' : 'https://';
+define('SITE_URL', ($url_prefix) . $_SERVER['HTTP_HOST'] . '/Payyourboostbill');
 $con = mysqli_connect('localhost', 'root', '', 'payyourboostbill');
